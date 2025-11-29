a7a1863998a700b62cdc4df109a01e74f4c7fb31
Madhava Jay <me@madhavajay.com>
2025-11-30 00:23:17 +1000

    Export CODON_STDLIB from bundled Codon

--- syqure/src/runner.rs
+++ syqure/src/runner.rs
@@ -60,6 +60,7 @@ impl Syqure {
             // Point CODON_PATH at the bundled Codon root so stdlib/plugins resolve correctly.
             let codon_root = ensure_bundle()?;
             std::env::set_var("CODON_PATH", &codon_root);
+            std::env::set_var("CODON_STDLIB", codon_root.join("stdlib"));
             if std::env::var_os("CODON_PLUGIN_PATH").is_none() {
                 std::env::set_var("CODON_PLUGIN_PATH", codon_root.join("plugins"));
             }
