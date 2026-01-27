230fbfd51490dbe72ec669b9b1d8aa2f4be1fc93
Madhava Jay <me@madhavajay.com>
2026-01-27 13:55:16 +1000

    lint

--- syqure/src/runner.rs
+++ syqure/src/runner.rs
@@ -212,8 +212,7 @@ fn ensure_libgmp_available(codon_root: &Path) {
         std::env::current_dir()
             .ok()
             .map(|d| d.join("bin/linux-x86/codon/lib/codon")),
-        std::env::var_os("SYQURE_CODON_INSTALL")
-            .map(|p| PathBuf::from(p).join("lib/codon")),
+        std::env::var_os("SYQURE_CODON_INSTALL").map(|p| PathBuf::from(p).join("lib/codon")),
     ];
 
     for candidate in candidates.into_iter().flatten() {
